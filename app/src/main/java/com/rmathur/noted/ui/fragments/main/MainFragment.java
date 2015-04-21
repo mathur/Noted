8e207d5f2566d5e4de2354fa77acb358913594f8
Rohan Mathur <rohan@rmathur.com>
2015-04-21 14:53:41 -0500

    Initial voice recognition
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- app/src/main/java/com/rmathur/noted/ui/fragments/main/MainFragment.java
+++ app/src/main/java/com/rmathur/noted/ui/fragments/main/MainFragment.java
@@ -1,16 +1,35 @@
 package com.rmathur.noted.ui.fragments.main;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.gc.materialdesign.views.ButtonRectangle;
 import com.rmathur.noted.R;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainFragment extends Fragment {
+
+    @InjectView(R.id.txtResult)
+    TextView txtResult;
+
+    @InjectView(R.id.btnStartSpeech)
+    ButtonRectangle btnStartSpeech;
+
+    private static final int REQUEST_OK = 1;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -22,6 +41,42 @@ public class MainFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.inject(this, view);
 
+        txtResult = (TextView) view.findViewById(R.id.txtResult);
+        btnStartSpeech = (ButtonRectangle) view.findViewById(R.id.btnStartSpeech);
+        btnStartSpeech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
         return view;
+    }
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQUEST_OK);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(this.getActivity().getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_OK && resultCode == Activity.RESULT_OK) {
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (result != null) {
+                txtResult.setText(result.get(0));
+            }
+        }
     }
 }
